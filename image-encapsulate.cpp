0466b284bc9d38892f2c40a956f07adb8e245add
G, Elango <elango.g@philips.com>
2024-04-16 18:23:16 +0530

    Image encapsulation

--- image-encapsulate.cpp
+++ image-encapsulate.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include "brightener.h"
 
-int main() {
-	auto image = std::make_unique<Image>(512, 512);
-	std::cout << "Brightening a 512 x 512 image\n";
-	ImageBrightener brightener(std::move(image));
-	if (brightener.ValidateImage()) {
-		int attenuatedCount = brightener.BrightenWholeImage();
-		std::cout << "Attenuated " << attenuatedCount << " pixels\n";
-		// Error... cannot access image here!
-		std::cout << "... in an image of: " << image->m_rows << " x " << image->m_columns << "\n";
-		return 0;
-	}
-	else {
-		std::cout << "Not a valid image... did nothing\n";
-		return 1;
-	}
+int main()
+{
+
+    const auto image = std::make_shared<Image>(512, 512);
+
+    if (image->IsValid())
+    {
+        std::cout << "Brightening a 512 x 512 image\n";
+        const ImageBrightener brightener(image);
+
+        const int attenuatedCount = brightener.BrightenWholeImage();
+        std::cout << "Attenuated " << attenuatedCount << " pixels\n";
+        std::cout << "... in an image of: " << image->getRows() << " x " << image->getColumns() << "\n";
+        return 0;
+    }
+    else 
+    {
+        std::cout << "Not a valid image... did nothing\n";
+        return 1;
+    }
 }
--- image.h
+++ image.h
@@ -2,16 +2,59 @@
 #include <cstdint>
 #include <iostream>
 
-class Image {
+constexpr uint16_t ImageRowsLimit = 1024;
+constexpr uint16_t ImageColumnsLimit = 1024;
+
+class Image
+{
 public:
-	Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
-		pixels = new uint8_t[m_rows * m_columns];
-	}
-	~Image() {
-		std::cout << "freeing pixel memory.\n";
-		delete[] pixels;
-	}
-	const uint16_t m_rows;
-	const uint16_t m_columns;
-	uint8_t* pixels; // max 1k x 1k image
+    Image(const uint16_t rows, const uint16_t columns)
+        : m_rows(rows)
+        , m_columns(columns)
+    {
+        const auto imageSize = static_cast<size_t>(m_rows * m_columns);
+        m_pixels = new uint8_t[imageSize];
+        memset(m_pixels, NULL, imageSize);
+    }
+
+    ~Image()
+    {
+        std::cout << "freeing pixel memory.\n";
+        delete[] m_pixels;
+    }
+
+    Image(const Image&) = delete;
+    Image& operator=(const Image&) = delete;
+    Image(const Image&&) = delete;
+    Image& operator=(const Image&&) = delete;
+
+
+    uint16_t getRows() const
+    {
+        return m_rows;
+    }
+
+    uint16_t getColumns() const
+    {
+        return m_columns;
+    }
+
+    uint8_t* getPixels() const
+    {
+        return m_pixels;
+    }
+
+    bool IsValid() const
+    {
+        if (m_columns <= ImageColumnsLimit && m_rows <= ImageRowsLimit) 
+        {
+            return true;
+        }
+        return false;
+    }
+
+private:
+    const uint16_t m_rows;
+    const uint16_t m_columns;
+    uint8_t* m_pixels; // max 1k x 1k image
 };
